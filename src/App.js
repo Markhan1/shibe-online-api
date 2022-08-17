5192e91e36c1a08b067e9633c38ee7e1ae700dfb
Markhan1 <markhan1@protonmail.com>
2022-08-17 14:38:18 +0800

    Full app shibe-online-api
    Full app of shibe-online-api. Open folder shibe-online-app in terminal and run 'npm run start', a local web-page will open, and press button to display 3 random images of Shibas.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { BASE_URL } from './api';
+
+const Parameters = {
+  count: 3,
+  urls: true,
+  httpsUrls: true,
+};
 
 function App() {
+  const [shibeLinks, setShibeLinks] = useState(null);
+
+  const FetchJson = () => {
+    fetch(
+      `${BASE_URL}?count=${Parameters.count}&urls=${Parameters.urls}&httpsUrls=${Parameters.httpsUrls}`
+    )
+      .then((response) => response.json())
+      .then((output) => {
+        setShibeLinks(output);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text">{"Click on this button bellow to fetch Shiba images! <3"}</div>
+      <button className="btn" onClick={FetchJson}>
+        click here
+      </button>
+      <ul style={{listStyleType: 'none'}}>
+        {shibeLinks &&
+          shibeLinks.map((element, index, array) => {
+            return (
+              <li key={index}>
+                {/* <a href={element}>{element}</a> */}
+                <img className="image" alt="Loading..." src={element} />
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
